8c5e7fda0ff2501f76de4f0611491615c4baf384
Cabrol Camille <cabrolc@f214-07.e-im2ag.ujf-grenoble.fr>
2021-11-30 12:49:16 +0100

    push pour tests

--- Client.c
+++ Client.c
@@ -10,9 +10,10 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+
 int main(int argc, char const *argv[])
 {
-     
+
      //Création d'une socket communication client
      int socketCommClient = socket(AF_INET, SOCK_STREAM, 0);
      if (socketCommClient == -1)
@@ -20,7 +21,9 @@ int main(int argc, char const *argv[])
           // fprintf(stderr, "Erreur dans la creation de la socket");
           perror("socket()");
           exit(-1);
-     } else{
+     }
+     else
+     {
           printf("Tout va bien création socket\n");
      }
 
@@ -30,14 +33,17 @@ int main(int argc, char const *argv[])
 
      socketServeur.sin_family = AF_INET;
      socketServeur.sin_port = htons(6067);
-     memcpy(&socketServeur.sin_addr.s_addr, infoServeur->h_addr_list[0], infoServeur->h_length);//affectation à l'aide de memcpy
+     memcpy(&socketServeur.sin_addr.s_addr, infoServeur->h_addr_list[0], infoServeur->h_length); //affectation à l'aide de memcpy
 
      printf("Adresse serveur : %s\n", inet_ntoa(socketServeur.sin_addr));
 
      //Connection du client au serveur
-     if(connect(socketCommClient, (struct sockaddr*) &socketServeur, sizeof(socketServeur)) == -1){
+     if (connect(socketCommClient, (struct sockaddr *)&socketServeur, sizeof(socketServeur)) == -1)
+     {
           perror("connect()");
-     }else{
+     }
+     else
+     {
           printf("Connection établie\n");
      }
 
@@ -46,8 +52,51 @@ int main(int argc, char const *argv[])
      write(socketCommClient, &tampon, sizeof(int));
 
      // while(read(socketCommClient, &tampon, sizeof(int)) == -1);
-	read(socketCommClient, &tampon, sizeof(int));
+     read(socketCommClient, &tampon, sizeof(int));
      printf("Valeur reçue : %d\n", tampon);
+
+
+     // Création de la boucle d'envoi des fichiers dans le cas où l'utilisateur veut déposer des fichiers sur le serveur
+     char** tabFichiersAEnvoyer = malloc(sizeof(char*) * 5); //tableau des numéros de fichiers à envoyer
+     char** tabFichiersRepertoireClient = malloc(sizeof(char*) * 5);
+     tabFichiersRepertoireClient[0] = malloc(sizeof(char) * 20);
+     tabFichiersRepertoireClient[1] = malloc(sizeof(char) * 20);
+     tabFichiersRepertoireClient[2] = malloc(sizeof(char) * 20);
+     tabFichiersRepertoireClient[3] = malloc(sizeof(char) * 20);
+     tabFichiersRepertoireClient[4] = malloc(sizeof(char) * 20);
+
+     tabFichiersAEnvoyer[0] = malloc(sizeof(char) * 20);
+     tabFichiersAEnvoyer[1] = malloc(sizeof(char) * 20);
+     tabFichiersAEnvoyer[2] = malloc(sizeof(char) * 20);
+     tabFichiersAEnvoyer[3] = malloc(sizeof(char) * 20);
+     tabFichiersAEnvoyer[4] = malloc(sizeof(char) * 20);
+
+     tabFichiersRepertoireClient[0] = "fleur.jpg";
+     tabFichiersRepertoireClient[1] = "arbre.jpg";
+     tabFichiersRepertoireClient[2] = "dauphin.jpg";
+     tabFichiersRepertoireClient[3] = "tortue.jpg";
+     tabFichiersRepertoireClient[4] = "lion.jpg";
+
+     int numFichier;  //numéro de fichier à envoyer choisi par l'utilisateur
+     int i = 0;
+     printf("Saisissez le numéro du fichier que vous voulez ajouter (-1 pour terminer) : \n");
+     scanf("%d\n", &numFichier);
+     while (i < 10 && numFichier != -1)
+     {
+          printf("Numéro de fichier : %d\n", numFichier);
+          tabFichiersAEnvoyer[i] = tabFichiersRepertoireClient[numFichier - 1]; //on stocke le numéro du fichier qu'on veut récupérer dans le tableau
+          i++;
+          printf("Saisissez le numéro du fichier que vous voulez ajouter (-1 pour terminer) : \n");
+          scanf("%d\n", &numFichier);
+     }
+
+     write(socketCommClient, &i, sizeof(int)); //envoi du nombre de fichiers que va recevoir le serveur au serveur
+
+     for (int j = 0; j < i; j++)
+     {
+          write(socketCommClient, tabFichiersAEnvoyer[j], sizeof(char*)); //envoi des fichiers au serveur
+     }
+     printf("J'ai fini d'envoyer les fichiers\n");
 
      return 0;
 }
--- Server.c
+++ Server.c
@@ -13,7 +13,8 @@
  * 
  * @param s //signal
  */
-void handler(int s) {
+void handler(int s)
+{
      wait(NULL);
 }
 
@@ -31,83 +32,103 @@ int main(int argc, char const *argv[])
      ac.sa_handler = handler;
      ac.sa_flags = SA_RESTART;
 
-     
-
      //Création d'une socket d'écoute
      int socketEcoute = socket(AF_INET, SOCK_STREAM, 0);
      if (socketEcoute == -1)
      {
           perror("socket()");
           exit(-1);
-     }else{
+     }
+     else
+     {
           printf("Tout va bien création socket\n");
      }
 
      //Attachement de la socket à un port
-     struct sockaddr_in socketClient;   
-     
+     struct sockaddr_in socketClient;
+
      socketClient.sin_family = AF_INET; // pointeur structure : (->) pour accès aux champs
      socketClient.sin_port = htons(6067);
      socketClient.sin_addr.s_addr = htonl(INADDR_ANY);
-     
-     if(bind(socketEcoute, (struct sockaddr*) &socketClient, sizeof(socketClient)) == -1){
+
+     if (bind(socketEcoute, (struct sockaddr *)&socketClient, sizeof(socketClient)) == -1)
+     {
           perror("bind()");
           exit(-1);
-     } else{
+     }
+     else
+     {
           printf("Tout va bien attachement socket\n");
      }
 
      //Ouverture du service
      //Maximum 5 connexions en attente
-     if(listen(socketEcoute, 5) == -1){
+     if (listen(socketEcoute, 5) == -1)
+     {
           perror("listen()");
           exit(-1);
-     } else{
+     }
+     else
+     {
           printf("Tout va bien ouverture\n");
      }
-     
+
      //Acceptation de la connection
      int len = sizeof(socketClient);
-     while (1){
+     while (1)
+     {
           int socketService;
-          if((socketService = accept(socketEcoute, (struct sockaddr*) &socketClient, &len)) == -1){ /* /!\l'appel de la fonction accept est bloquant/!\ */
+          if ((socketService = accept(socketEcoute, (struct sockaddr *)&socketClient, &len)) == -1)
+          { /* /!\l'appel de la fonction accept est bloquant/!\ */
                perror("accept()");
                exit(-1);
-          } else {
+          }
+          else
+          {
                printf("Accepte connection ok\n"); // Ici on a eu aucun affichage car on avait pas mit de \n
                                                   // penser à en mettre pour flush le buffer et afficher sur la sortie standard
           }
 
           int tampon = -1;
-          switch(fork()) {
-               case -1:
-                    //erreur
-                    perror("fork()");
-                    exit(-1);
-                    break;
-               
-               case 0:
-                    //comportement du fils
+          switch (fork())
+          {
+          case -1:
+               //erreur
+               perror("fork()");
+               exit(-1);
+               break;
 
-                    close(socketEcoute); //fermeture du descripteur de fichier
-                    
-                    signal(SIGCHLD, SIG_DFL); //redéfinition (à défaut) du comportement du signal SIGCHLD pour ne pas hériter de celui du père = réinitialisation
+          case 0:
+               //comportement du fils
 
-                    read(socketService, &tampon, sizeof(int));
-                    printf("Valeur lue : %d\n", tampon);
+               close(socketEcoute); //fermeture du descripteur de fichier
 
-                    tampon += 3;
-                    write(socketService, &tampon, sizeof(int));
+               signal(SIGCHLD, SIG_DFL); //redéfinition (à défaut) du comportement du signal SIGCHLD pour ne pas hériter de celui du père = réinitialisation
 
-                    exit(0);
-                    break;
+               read(socketService, &tampon, sizeof(int));
+               printf("Valeur lue : %d\n", tampon);
 
-               default:
-                    //comportement du père
-                    break;
+               tampon += 3;
+               write(socketService, &tampon, sizeof(int));
+
+               int nbFichiersALire = 0;
+               while(read(socketService, &nbFichiersALire, sizeof(int)) == -1);
+
+               int i = 0;
+               char* tamponString;
+               while(i < nbFichiersALire) {
+                    while(read(socketService, tamponString, sizeof(char*)) == -1);
+                    printf("Valeur lue : %s\n", tamponString);
+                    i++;
+               }
+               printf("Enregistrement terminé\n");
+               exit(0);
+               break;
+
+          default:
+               //comportement du père
+               break;
           }
-
-          
      }
 
      return 0;
