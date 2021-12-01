1635573300883d847bd615f6a3c7395064f3a518
Djey26 <jeremiegourdon2504gmail.com>
2021-12-01 22:12:16 +0100

    "Fichier Supprimer"

--- Source/Reception.h
+++
@@ -1,9 +0,0 @@
-#ifndef RECEPTION_H
-#define RECEPTION_H
-
-/**
- * @brief 
- * Pour le moment rien
- */
-void receptionFichier(int socketCommClient);
-#endif
--- run.sh
+++
@@ -1,7 +0,0 @@
-gcc -c -o Objets/Envoi.o Source/Envoi.c
-gcc -c -o Objets/Reception.o Source/Reception.c
-gcc -c -o Objets/Client.o Source/Client.c
-gcc -c -o Objets/Server.o Source/Server.c
-gcc -c -o Objets/Transfert.o Source/Transfert.c
-gcc -g -o Exec/client Objets/Client.o Objets/Envoi.o Objets/Reception.o Objets/Transfert.o
-gcc -g -o Exec/server Objets/Server.o Objets/Transfert.o
